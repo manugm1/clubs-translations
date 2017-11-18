348ec0a051f8c1ffd92cfe1144a9e6c68ae61ae1
Manuel <garciamenarguez@gmail.com>
2017-11-18 13:34:19 +0000

    Creación de la vista de edición de un idioma.

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -83,7 +83,9 @@ class LanguageController extends Controller
      */
     public function edit($id)
     {
-        //
+        //Objeto correspondiente
+        $language = Language::find($id);
+        return view('languages.edit')->with('var', $language);
     }
 
     /**
@@ -95,7 +97,33 @@ class LanguageController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        //Validación
+        $reglas = array(
+            'name' => 'required|string',
+            'locale' => 'required|string',
+        );
+
+        $validator = Validator::make(Input::all(), $reglas);
+
+        //Obtenemos objeto
+        $objeto = Language::find($id);
+
+        //Comprobamos reglas
+        if ($validator->fails()) { //Si falla
+            Session::flash('message', "Hay algún error en los datos introducidos.");
+            return redirect()->route('languages.edit', ['id' => $objeto->id])->withErrors($validator)
+                ->withInput();
+        } else {
+            
+            // Si no, guardamos el objeto en la base de datos
+            $objeto->name = Input::post('name');
+            $objeto->locale = Input::post('locale');
+            $objeto->save();
+
+            //Redireccionamos a vista listado
+            Session::flash('message', 'Idioma editado correctamente');
+            return Redirect::to('languages');
+        }
     }
 
     /**
---
+++ resources/views/languages/edit.blade.php
@@ -0,0 +1,79 @@
+@extends('master')
+
+@section('pagetitle', 'Editar Idioma {{$var->id}}')
+
+@section('headercontent')
+<style>
+    #tabla tr *{
+        text-align: center !important;
+    }
+</style>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+    Idiomas
+    <small>Editar un idioma</small>
+    </h1>
+    <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+    <li class="active">Editar un idioma</li>
+    </ol>
+</section>
+@stop
+@section('content')
+
+<section class="content">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-xs-12">
+            @if ($errors->any())
+                <div class="callout callout-danger">
+                    <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                    </ul>
+                </div>
+            @endif
+                <div class="box box-success">
+                    <div class="box-header">
+                        <h3 class="box-title">Editar idioma <strong>{{$var->name}}</strong></h3>
+                    </div><!-- /.box-header -->
+                    <!-- form start -->
+                    {!! Form::open(['route' => ['languages.update', $var->id], 'method' => 'PUT', 'class' => 'form-horizontal']) !!}
+                    <div class="box-body">
+                        <div class="form-group">
+                            <label for="titulo" class="col-sm-3 control-label">ID</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" disabled="disabled" name="id" id="id" placeholder="ID" value="{{$var->id}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="titulo" class="col-sm-3 control-label">Nombre</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Nombre" value="{{$var->name}}">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="autor" class="col-sm-3 control-label">Locale</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" name="locale" id="locale" placeholder="Locale" value="{{$var->locale}}">
+                            </div>
+                        </div>
+                    </div><!-- /.box-body -->
+                    <div class="box-footer">
+                        <button type="reset" class="btn btn-default">Reset</button>
+                        <button type="submit" class="btn btn-success pull-right">Editar</button>
+                    </div><!-- /.box-footer -->
+                    {!! Form::close() !!}
+                </div><!-- /.box -->
+            </div><!-- ./col -->
+        </div><!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+            <section class="col-lg-1 col-lg-offset-5">
+                <a class="btn btn-small btn-default" href="{{ URL::to('languages') }}">Volver</a>
+            </section><!-- right col -->
+        </div><!-- /.row (main row) -->
+    </section><!-- /.content -->
+    @stop
