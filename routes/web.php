4d33c9723bc408808c50524132b935fc8c7e4597
Manuel <garciamenarguez@gmail.com>
2017-11-18 10:17:55 +0000

    Ajustes en las rutas tanto para clubs como idiomas. De momento solo métodos CRUDs.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rutas de la web interna
+Route::resource('clubs', 'ClubController');
+Route::resource('languages', 'LanguageController');
